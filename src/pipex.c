e97c07beaeaf559cb0600c24d5773798e2de6e7b
Sheryl Slowpoke <sslowpok@pl-k1.msk.21-school.ru>
2022-02-22 19:00:48 +0300

    txt

--- src/pipex.c
+++ src/pipex.c
@@ -6,14 +6,20 @@
 /*   By: sslowpok <sslowpok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 17:21:45 by sslowpok          #+#    #+#             */
-/*   Updated: 2022/02/21 18:39:13 by sslowpok         ###   ########.fr       */
+/*   Updated: 2022/02/22 18:00:48 by sslowpok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
+void	child_process(int f1, t_cmd cmd1)
+{
+	
+}
+
 void	pipex(int f1, int f2, char **argv, char **envp)
 {
+	// cmd[1] is child process, cmd[0] is parent process
 	int		end[2];
 	pid_t	parent;
 
@@ -21,7 +27,7 @@ void	pipex(int f1, int f2, char **argv, char **envp)
 	parent = fork();
 	if (parent < 0)
 		return (perror("Fork: "));
-	// if (!parent)
+	// if (!parent) // if fork returns 0, we are in the child process
 	// 	child_process(f2, cmd2);
 	// else
 	// 	parent_process(f2, cmd2);
