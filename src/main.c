01093f13f5c634bcce20bc666323e0cce7a70151
Sheryl Slowpoke <sslowpok@mi-s5.msk.21-school.ru>
2022-03-07 11:36:46 +0300

    just a save

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sslowpok <sslowpok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 10:07:02 by alex              #+#    #+#             */
-/*   Updated: 2022/03/02 17:19:47 by alex             ###   ########.fr       */
+/*   Updated: 2022/03/07 11:35:54 by sslowpok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,30 @@ void	inp_error(void)
 }
 
 /*
-** ./pipex file1 cmd1 | cmd2 > file2
-** ./pipex infile "ls -l" | "wc -l" outfile
+** ./pipex file1 cmd1 cmd2 file2
+** ./pipex infile "ls -l" "wc -l" outfile
 */
 
-void	pipex(int f1, int f2)
+void	child_one(int )
+
+void	pipex(char **argv, char **envp)
 {
+	t_child	child;
+
+	if (pipe(child.fd) == -1)
+		exit(1);
+	// child.fd[0] = open(O_RDONLY, 0777);
+	child.child1 = fork();
+	if (child.child1 < 0)
+		exit(1);
+	else if (child.child1 == 0)
 	
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_child	child;
-
-	
+	// if (argc < 5)
+	// 	inp_error();
+	pipex(argv, envp);
 	return (0);
 }
